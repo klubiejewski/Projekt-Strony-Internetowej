883658944c586d940fc19448d69651297c4e74dd
klubiejewski <122455137+klubiejewski@users.noreply.github.com>
2024-01-11 14:51:52 +0100

    Add files via upload

---
+++ amongus.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portal Wędkarski</title>
+    <link rel="stylesheet" href="amongus.css">
+</head>
+<body>
+    <header>
+        <h1>Portal Wędkarski</h1>
+    </header>
+    <section class="lista">
+        <ul>
+            <li>
+                <a href="suslogowanie.php">
+                    Logowanie
+                </a>
+            </li>
+            <li>
+                <a href="susrejestracja.php">
+                    Rejestracja
+                </a>
+            </li>
+            <li>
+                <a href="susustawienia.html">
+                Ustawienia
+                </a>
+            </li>
+            <li>
+                <a href="amongus.php">
+                Strona Główna
+                </a>
+            </li>
+        </ul>
+    </section>
+
+    <section>
+        <img alt="susicon" src="susicona.png" class="ikona">
+        <img alt="susryby" src="susryby.jpg" class="ryby">
+        <p class="nazwa">Kacper</p>
+        <p class="tekst1">Zobaczcie jakie ostatnio ze szwagrem Miłoszem złowilismy ryby! 1,5 metra</p><br><br><br>
+    </section>
+    <section>
+        <img alt="susicon" src="susicona.png" class="ikona">
+        <p class="nazwa">Szymon</p><br> 
+        <p class="tekst2">Niezłe łowy Kacper</p><br><br>
+    </section>
+  
+
+
+
+</body>
+</html>
---
+++ suslogowanie.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logowanie</title>
+    <link rel="stylesheet" href="amongus.css">
+</head>
+<body>
+    <header>
+        <h1>Portal Wędkarski</h1>
+    </header>
+    <section class="lista">
+        <ul>
+            <li>
+                <a href="suslogowanie.php">
+                    Logowanie
+                </a>
+            </li>
+            <li>
+                <a href="susrejestracja.php">
+                    Rejestracja
+                </a>
+            </li>
+            <li>
+                <a href="susustawienia.html">
+                Ustawienia
+                </a>
+            </li>
+            <li>
+                <a href="amongus.php">
+                Strona Główna
+                </a>
+            </li>
+        </ul>
+    </section>
+
+    <section>
+        <form method="post" action="">
+            <label>Nazwa użytkownika</label><input type="text" name="Nazwa_uzytkownika"><br>
+            <label>E-mail</label><input type="text" name="E-mail"><br>
+            <label>Hasło</label><input type="text" name="Haslo"><br>
+            <button type="submit" value="WYŚLIJ">WYŚLIJ</button>
+        </form>
+    </section>
+</body>
+</html>
